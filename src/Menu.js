4db0617f9f1af9b191eb63c850d92e99f74ef546
touggani <35734200+touggani@users.noreply.github.com>
2021-04-16 14:35:30 +0200

    exercise

--- src/Menu.js
+++ src/Menu.js
@@ -3,15 +3,9 @@ import { Link } from "react-router-dom";
 
 import "./styles.css";
 
-const Menu = props => {
+const Menu = (props) => {
   return (
     <div>
-      <Link to="/exercice/1">Ex1</Link>
-      <Link to="/exercice/2">Ex2</Link>
-      <Link to="/exercice/3">Ex3</Link>
-      <Link to="/exercice/4">Ex4</Link>
-      <Link to="/exercice/5">Ex5</Link>
-      <Link to="/exercice/6">Ex6</Link>
       <Link to="/country/France">France</Link>
       <Link to="/country/Germany">Allemagne</Link>
       <Link to="/country/Canada">Canada</Link>
